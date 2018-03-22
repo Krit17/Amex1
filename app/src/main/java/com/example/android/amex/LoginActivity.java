fb2508baf374dc7bdcbce02fdd9f3ff2f122991a
Krithika <krithikaa1705@gmail.com>
2018-03-22 21:16:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/amex/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.amex;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button= (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,DetailedActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
